b17f2e85c49d9ca0e7c927fb4d7a66cb93333337
Sam Chatfield <samchatfield97@gmail.com>
2019-03-25 16:05:48 +0000

    Add build to docker compose file. Implement changing user.

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,52 @@ import React from 'react';
 import './App.css';
 import Dropzone from 'react-dropzone';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCloudUploadAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleRight, faCloudUploadAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-library.add(faCloudUploadAlt, faTrashAlt);
+library.add(faArrowAltCircleRight, faCloudUploadAlt, faTrashAlt);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    const initialUserId = '1234567';
+
     this.state = {
       files: [],
+      userId: initialUserId,
+      userIdValue: initialUserId,
     };
 
     this.handleDrop = this.handleDrop.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleUserChangeSubmit = this.handleUserChangeSubmit.bind(this);
   }
 
   componentDidMount() {
-    fetch('/api/user/1234567/files')
+    const { userId } = this.state;
+
+    fetch(`http://localhost/api/user/${userId}/files`)
       .then(response => response.json())
       .then(data => this.setState({ files: data }));
   }
 
   handleDrop(acceptedFiles) {
-    console.log('Handle Drop');
-    console.log(acceptedFiles);
     const formData = new FormData();
 
     acceptedFiles.forEach((file) => {
       formData.append('file', file);
     });
 
-    fetch('/api/user/1234567/files', {
+    const { userId } = this.state;
+
+    fetch(`http://localhost/api/user/${userId}/files`, {
       method: 'POST',
       body: formData,
     })
       .then(response => response.json())
       .then((data) => {
-        console.log(`POST res:\n${JSON.stringify(data)}`);
         const newFiles = data.files;
-        console.log(`newFiles:\n${JSON.stringify(newFiles)}`);
         this.setState({ files: newFiles });
       });
   }
@@ -49,7 +56,8 @@ class App extends React.Component {
     console.log('Handle Delete');
     console.log(`Delete file ${fileId}`);
 
-    fetch(`/api/user/1234567/files/${fileId}`, {
+    const { userId } = this.state;
+    fetch(`http://localhost/api/user/${userId}/files/${fileId}`, {
       method: 'DELETE',
     })
       .then(response => response.json())
@@ -61,11 +69,76 @@ class App extends React.Component {
       });
   }
 
+  handleUserChange(event) {
+    const newUserId = event.target.value;
+
+    this.setState({
+      userIdValue: newUserId,
+    });
+  }
+
+  handleUserChangeSubmit(event) {
+    event.preventDefault();
+
+    const { userIdValue } = this.state;
+
+    if (!userIdValue) {
+      this.setState({
+        files: [],
+        userId: userIdValue,
+      });
+    } else {
+      fetch(`http://localhost/api/user/${userIdValue}/files`)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          this.setState({
+            files: data,
+            userId: userIdValue,
+          });
+        })
+        .catch(() => {
+          this.setState({
+            files: [],
+            userId: userIdValue,
+          });
+        });
+    }
+  }
+
   render() {
-    const { files } = this.state;
+    const { files, userIdValue } = this.state;
     return (
       <div className="App bg-dark">
         <h1 className="Header">SDS File Upload</h1>
+        <div className="UserSelect card bg-light">
+          <form
+            className="form-inline"
+            onSubmit={this.handleUserChangeSubmit}
+            autoComplete="off"
+          >
+            <label htmlFor="userField"> User ID:&nbsp;</label>
+            <div className="input-group">
+              <input
+                id="userField"
+                className="form-control"
+                type="text"
+                name="userId"
+                value={userIdValue}
+                onChange={this.handleUserChange}
+              />
+              <div className="input-group-append">
+                <button className="btn btn-dark" type="submit">
+                  <FontAwesomeIcon icon="arrow-alt-circle-right" size="lg" />
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
         <Dropzone onDrop={this.handleDrop}>
           {({ getRootProps, getInputProps }) => (
             <section>
